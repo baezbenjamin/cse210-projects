412dff48e07fa00407f9a4c54d300fd5babc6634
baezbenjamin <baezbenjamin03@gmail.com>
2025-05-20 22:54:54 -0300

    W03 Learning Activity

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(6);
+        Fraction f3 = new Fraction(6, 7);
+
+        f3.SetTop(3);
+        f3.SetBottom(4);
+
+        // Console.WriteLine(f1.GetTop());
+        // Console.WriteLine(f2.GetBottom());
+        // Console.WriteLine(f3.GetTop());
+        // Console.WriteLine(f3.GetBottom());
+
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
     }
 }
