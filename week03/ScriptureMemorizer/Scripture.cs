49a603d4b74d2b89b1fdb67bfca5358965d1fdee
baezbenjamin <baezbenjamin03@gmail.com>
2025-05-31 14:39:50 -0300

    Scripture Memorizer Finished

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -1,4 +1,52 @@
-// public class Scripture
-// {
-//     private 
-// }
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = [];
+        string[] wordList = text.Split(" ");
+        for (int i = 0; i < wordList.Length; i++)
+        {
+            Word word = new Word(wordList[i]);
+            _words.Add(word);
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        for (int i = 0; i < numberToHide; i++)
+        {
+            Random random = new Random();
+            int index = random.Next(_words.Count);
+            _words[index].Hide();
+        }
+
+    }
+
+    public string GetDisplayText()
+    {
+        string fullText = $"{ _reference.GetDisplayText()}";
+        foreach (Word word in _words) {
+            fullText = fullText + " " + word.GetDisplayText();
+        }
+        return fullText;
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        bool completelyHidden = false;
+        List<bool> conditions = new List<bool>{};
+        for (int i = 0; i < _words.Count; i++)
+        {
+            conditions.Add(_words[i].IsHidden());
+        }
+        if (conditions.All(c => c))
+        {
+            completelyHidden = true;
+        }
+        return completelyHidden;
+    }
+}
--- week03/ScriptureMemorizer/Word.cs
+++ week03/ScriptureMemorizer/Word.cs
@@ -1,1 +1,34 @@
-// "Trust in the Lord with all thine heart; and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths."
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+        string hiddenText = "";
+        for (int i = 0; i < _text.Length; i++)
+        {
+            hiddenText = hiddenText + "_";
+        }
+        _text = hiddenText;
+        _isHidden = true;
+    }
+    public void Show()
+    {
+        _isHidden = false;
+    }
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+    public string GetDisplayText()
+    {
+        return _text;
+    }
+}
