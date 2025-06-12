09fcab9ba0058f9f0a13ef857c6b246640b7aaf2
baezbenjamin <baezbenjamin03@gmail.com>
2025-06-12 00:26:27 -0300

    Mindfulness Design

---
+++ week05/Mindfulness/ListingActivity.cs
@@ -0,0 +1,22 @@
+public class ListingActivity : Activity
+{
+    protected int _count;
+    protected List<string> _prompts;
+    public ListingActivity()
+    {
+
+    }
+    public void Run()
+    {
+
+    }
+    public void GetRandomPrompt()
+    {
+
+    }
+
+    public List<string> GetListFromUser()
+    {
+        return [];
+    }
+}
---
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -0,0 +1,32 @@
+public class ReflectingActivity : Activity
+{
+    protected List<string> _prompts;
+    protected List<string> _questions;
+    
+    public ReflectingActivity()
+    {
+
+    }
+    public void Run()
+    {
+
+    }
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+    public string GetRandomQuestion()
+    {
+        return "";
+    }
+
+    public void DisplayPrompt()
+    {
+
+    }
+
+    public void DisplayQuestion()
+    {
+
+    }
+}
